79318a40d94893c1ad541a9d992abac2a74d74b1
Roy Garrett <roygarrett116@outlook.com>
2022-03-31 20:02:58 -0700

    Added LinkedIn

--- src/components/AboutSection/AboutData.js
+++ src/components/AboutSection/AboutData.js
@@ -8,6 +8,7 @@ export const About = {
   topLine: 'About Me',
   headLine: 'Computer Science and Business: MIS',
   description: "Third year student at The Master's University Passionate Coder, Eager to Learn",
+  buttonLabel: 'LinkedIn',
   imgStart: false, //Toggle for image to switch sides
   img: aboutPic,
   alt: 'about-pic',
--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ButtonLink} from '../ButtonLinkElements';
 import {
   InfoContainer,
   InfoWrapper,
@@ -10,10 +11,11 @@ import {
   Heading,
   Subtitle,
   Img,
-  ImgWrap
+  ImgWrap,
+  BtnWrap
 } from './AboutElements';
 
-const AboutSection = ({lightBg, id, imgStart, topLine, lightText, headLine, description, img, alt}) => {
+const AboutSection = ({lightBg, id, imgStart, topLine, lightText, headLine, description, img, alt, buttonLabel, primary, dark, dark2}) => {
   return (
     <>
       <InfoContainer lightBg={lightBg} id={id}>
@@ -24,6 +26,20 @@ const AboutSection = ({lightBg, id, imgStart, topLine, lightText, headLine, desc
                 <TopLine>{topLine}</TopLine>
                 <Heading lightText={lightText}>{headLine}</Heading>
                 <Subtitle lightText={lightText}>{description}</Subtitle>
+                <BtnWrap>
+                  <ButtonLink
+                    href='https://www.linkedin.com/in/roy-garrett-b6ab60224/'
+                    target='_blank'
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact="true"
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >{buttonLabel}</ButtonLink>
+                </BtnWrap>
               </TextWrapper>
             </Column1>
             <Column2>
