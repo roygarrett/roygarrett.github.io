d21224c953d16a27157a5240a8dbf7229c044f04
Roy Garrett <roygarrett116@outlook.com>
2025-01-12 23:28:36 -0800

    2025 update

--- src/components/AboutSection/AboutData.js
+++ src/components/AboutSection/AboutData.js
@@ -6,8 +6,8 @@ export const About = {
   lightText: false,
   lightTextDesc: false,
   topLine: 'About Me',
-  headLine: 'Computer Science: CS/AI/IS & Business: MIS',
-  description: "Fifth year student at The Master's University Passionate Coder, Eager to Learn",
+  headLine: 'Software Engineer, Computer Scientist, M.S. Candidate',
+  description: "Software Engineer for military aircraft at JT4 LLC, M.S. Candidate in CS at Georgia Institute of Technology",
   buttonLabel: 'LinkedIn',
   imgStart: false, //Toggle for image to switch sides
   img: aboutPic,
